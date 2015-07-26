2eac96026bd61d8b832bd92ac9923480c71f2fe8
unjo <unnjo63@hotmail.co.jp>
2015-07-26 10:59:28 +0900

    Curing Bobby Tables

--- forumdb.py
+++ forumdb.py
@@ -24,6 +24,7 @@ def GetAllPosts():
 def AddPost(content):
     DB =psycopg2.connect("dbname=forum")
     c = DB.cursor()
-    c.execute("INSERT INTO posts (content) VALUES ('%s')" % content)
+    c.execute("INSERT INTO posts (content) VALUES (%s)",
+              (content,))
     DB.commit()
     DB.close()
