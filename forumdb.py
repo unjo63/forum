eaade70f4f6755f40a0f0f22df902619811e6b8f
unjo <unnjo63@hotmail.co.jp>
2015-07-26 10:34:34 +0900

    Python DB-API

---
+++ forumdb.py
@@ -0,0 +1,29 @@
+#
+# Database access functions for the web forum.
+# 
+
+import psycopg2
+
+## Get posts from database.
+def GetAllPosts():
+    DB =psycopg2.connect("dbname=forum")
+    c = DB.cursor()
+    c.execute("SELECT time, content FROM posts ORDER BY time DESC")
+    '''Get all the posts from the database, sorted with the newest first.
+
+    Returns:
+      A list of dictionaries, where each dictionary has a 'content' key
+      pointing to the post content, and 'time' key pointing to the time
+      it was posted.
+    '''
+    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in c.fetchall()]
+    DB.close()
+    return posts
+
+## Add a post to the database.
+def AddPost(content):
+    DB =psycopg2.connect("dbname=forum")
+    c = DB.cursor()
+    c.execute("INSERT INTO posts (content) VALUES ('%s')" % content)
+    DB.commit()
+    DB.close()
